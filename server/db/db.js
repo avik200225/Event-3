2e859a7e896c24f5ef78a05277a28ec430efeb32
avik200225 <157623260+avik200225@users.noreply.github.com>
2024-03-22 16:27:05 +0530

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/FinalProject");
+    await mongoose.connect("mongodb://127.0.0.1:27017/Event-Management");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Connection to MongoDB failed:", error);
